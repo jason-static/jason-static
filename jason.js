07589b98910450451ad3745a0e0de5dcb6515f6d
Wes Lord <weslord@gmail.com>
2020-12-08 08:13:07 -0800

    WIP - recursively traverse the pages object to find route definitions

--- jason.js
+++ jason.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
-import * as fs from 'fs'
+import * as fs from "fs"
 
-const dataPath = process.argv[2]
+const dataPath = process.argv[2] || "data.json"
+const pagesPath = process.argv[3] || "pages.json"
+
 const data = JSON.parse(fs.readFileSync(dataPath))
+const pages = JSON.parse(fs.readFileSync(pagesPath))
 
-console.log(data)
+const recursePages = (dir, parent = "") => {
+    Object.keys(dir, parent).map((key) => {
+        if (key.startsWith("/")) {
+            if (typeof dir[key] === "object") {
+                recursePages(dir[key], parent+key)
+            }
+        }
+
+        if (key === "template") {
+            console.log(parent)
+        }
+  })
+}
+
+recursePages(pages)
