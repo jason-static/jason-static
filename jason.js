617054be9de70dd4b316d0ca9f7292d62991dfef
Wes Lord <weslord@gmail.com>
2020-12-08 09:07:52 -0800

    WIP - generate paths from name and data array

--- jason.js
+++ jason.js
@@ -12,9 +12,10 @@ const recursePages = (dir, parent = "", variables = {}) => {
         if (key.startsWith("/")) {
             const pathData = dir[key]
 
+
             if (typeof pathData === "object" /* "always" true for child of /key */) {
                 const localVariables = {}
-            
+
                 Object.keys(pathData).map(key => {
                     if (key.startsWith("_")) {
                         const name = key.slice("_".length)
@@ -23,13 +24,24 @@ const recursePages = (dir, parent = "", variables = {}) => {
                     }
                 })
 
-                recursePages(pathData, parent + key)
+                const pathVars = key.match(/\[.+?\]/g)
+                if (pathVars) {
+                    pathVars.map(stringToReplace => {
+                        const replacementStrings = localVariables[stringToReplace.slice(1,-1)]
+                        replacementStrings.map(newString => {
+                            const newKey = key.replace(stringToReplace, newString)
+                            recursePages(pathData, parent + newKey)
+                        })
+                    })
+                } else {
+                    recursePages(pathData, parent + key)
+                }
             }
         }
 
 
         if (key === "template") {
-            console.log(parent)
+            console.log(parent, '\t', dir[key])
             return
         }
   })
@@ -40,7 +52,3 @@ recursePages(pages)
 // todo:
 //   parse paths into work-list
 //   use work-list to generate pages
-
-const workList = {
-
-}
