03b1005688e39d3abb05374a324d28e97babb6e0
Wes Lord <weslord@gmail.com>
2020-12-08 08:43:46 -0800

    WIP - checkpoint

--- jason.js
+++ jason.js
@@ -7,18 +7,40 @@ const pagesPath = process.argv[3] || "pages.json"
 const data = JSON.parse(fs.readFileSync(dataPath))
 const pages = JSON.parse(fs.readFileSync(pagesPath))
 
-const recursePages = (dir, parent = "") => {
-    Object.keys(dir, parent).map((key) => {
+const recursePages = (dir, parent = "", variables = {}) => {
+    Object.keys(dir, parent).forEach((key) => {
         if (key.startsWith("/")) {
-            if (typeof dir[key] === "object") {
-                recursePages(dir[key], parent+key)
+            const pathData = dir[key]
+
+            if (typeof pathData === "object" /* "always" true for child of /key */) {
+                const localVariables = {}
+            
+                Object.keys(pathData).map(key => {
+                    if (key.startsWith("_")) {
+                        const name = key.slice("_".length)
+                        const path = pathData[key].slice("data.".length)
+                        localVariables[name] = data[path]
+                    }
+                })
+
+                recursePages(pathData, parent + key)
             }
         }
 
+
         if (key === "template") {
             console.log(parent)
+            return
         }
   })
 }
 
 recursePages(pages)
+
+// todo:
+//   parse paths into work-list
+//   use work-list to generate pages
+
+const workList = {
+
+}
