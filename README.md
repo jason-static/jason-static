290a0d462ea7080b08dd612832967ee5205415c9
Wes Lord <weslord@gmail.com>
2020-12-11 12:40:19 -0800

    Finalize first draft of parser

--- README.md
+++ README.md
@@ -56,3 +56,41 @@ If an array of data exists, that array must be assigned to a variable so that th
     "/[company.employees.name].html": "templatefile.html"
 }
 ```
+
+## UNKNOWN / FUTURE ##
+(not yet supported)
+While it's not possible to reference a value via a child array of a named variable, it may be possible to have array assignment via variable parent...
+
+```json
+{
+    "data": {
+        "employees": [
+            { "name": "greg" },
+            { "name": "bill" }
+        ]
+    },
+    "_employee_name": "data.employees.name",
+    "/[employee.name].html": "templatefile.html"
+}
+```
+
+## Notes:
+ validate pathPatterns
+ - does data exist at path?
+   - accepatable:
+     - direct data reference to usable string
+       - (eg /[data.company.title])
+     - variable assigned to useable string
+       - (eg /[title].html :: _title: data.company.title)
+     - subpath of variable assigned to useable string
+       - (eg /[company.title].html :: _company: data.company)
+     - variable assigned to array of usable strings
+       - (eg /[name].html :: _name: data.company.employee_names)
+     - subpath of variable assigned to array of objects where that subpath is a direct path to usable string
+       - (eg /[employee.name].html :: _employee: data.company.employees)
+   - not acceptable:
+     - path containing an array not directly assigned to a variable
+       - (eg /[data.company.employee.name].html)
+       - (eg /[company.employee.name].html :: [_comapny]: data.company)
+       - (eg /[company.employee_names].html :: [_company]: data.company)
+ - ultimate data type must be valid url st
--- jason.js
+++ jason.js
@@ -10,7 +10,7 @@ const data = JSON.parse(fs.readFileSync(dataPath))
 const pages = JSON.parse(fs.readFileSync(pagesPath))
 
 const getDottedPath = (data, dataPath) => {
-    return dataPath.split('.').reduce((data, pathSeg) => {
+    return dataPath.split(".").reduce((data, pathSeg) => {
         return data[pathSeg]
     }, data)
 }
@@ -18,7 +18,7 @@ const getDottedPath = (data, dataPath) => {
 const recursePaths = (parentPath, parentPathData, context) => {
     const templateFile = parentPathData["template"]
     if (typeof templateFile === "string") {
-        console.log(parentPath, "\t", templateFile)
+        console.log(templateFile, "\t", parentPath)
     }
 
     Object.keys(parentPathData)
@@ -28,111 +28,89 @@ const recursePaths = (parentPath, parentPathData, context) => {
             const innerVars = {}
 
             Object.keys(pathData)
-                .filter(innerKey => innerKey.startsWith("_"))
+                .filter((innerKey) => innerKey.startsWith("_"))
                 .forEach((innerKey) => {
                     const name = innerKey.slice("_".length)
                     const dataPath = pathData[innerKey]
                     innerVars[name] = getDottedPath(context, dataPath)
-            })
+                })
 
             const paths = []
 
             const pathPatterns = key.match(/\[.+?\]/g)
             if (pathPatterns) {
-                const innerPatterns = pathPatterns.map(outerPattern => {
-                    const pattern = outerPattern.slice(1,-1)
-                    const replacement = getDottedPath({ ...innerVars, ...context}, pattern)
-                    const replacements = typeof(replacement) === 'string' ? [replacement] : replacement
-                    return {
-                        [pattern]: replacements
-                    }
-                })
 
-                console.log('ip', innerPatterns)
+                // Step 1: Define URL patterns from URL template:
 
-
-
-                // validate pathPatterns
-                // - does data exist at path?
-                //   - accepatable:
-                //     - direct data reference to usable string
-                //       - (eg /[data.company.title])
-                //     - variable assigned to useable string
-                //       - (eg /[title].html :: _title: data.company.title)
-                //     - subpath of variable assigned to useable string
-                //       - (eg /[company.title].html :: _company: data.company)
-                //     - variable assigned to array of usable strings
-                //       - (eg /[name].html :: _name: data.company.employee_names)
-                //     - subpath of variable assigned to array of objects where that subpath is a direct path to usable string
-                //       - (eg /[employee.name].html :: _employee: data.company.employees)
-                //   - not acceptable:
-                //     - path containing an array not directly assigned to a variable
-                //       - (eg /[data.company.employee.name].html)
-                //       - (eg /[company.employee.name].html :: [_comapny]: data.company)
-                //       - (eg /[company.employee_names].html :: [_company]: data.company)
-                // - ultimate data type must be valid url string
-
-
-
-                // const newPaths = innerPatterns.reduce((urlDef, {pattern, replacements}) => {
-                //     return urlDef.replace(`[${pattern}]`, replacements[0])
-                // }, key)
-
-                // console.log('\tnp',newPaths)
-
-
-
-                pathPatterns.map((outerPattern) => {
-                    const innerPattern = outerPattern.slice(1, -1)
-                    const replacement = getDottedPath({ ...innerVars, ...context}, innerPattern)
-
-                    if(typeof(replacement) === 'string') {
-                        const currentPath = key.replace(outerPattern, replacement)
-                        recursePaths(parentPath + currentPath, pathData, {
-                            ...innerVars,
-                            ...context
-                        })
-                    } else {
-                        replacement.map((replacementString) => {
-                            const currentPath = key.replace(outerPattern, replacementString)
-                            // TODO: pass specific variable instance as context...
-                            // TODO: move this outside...
-                            // recursePaths(parentPath + currentPath, pathData, {
-                            //     ...innerVars,
-                            //     ...context
-                            // })
-                        })
-                    }
-                })
-
-
-            } else {
-                paths.push(
-                    {
-                        path: parentPath + key,
-                        context: {
-                            ...innerVars,
-                            ...context
-                        }
-                    }
+                const innerPatterns = pathPatterns.map((outerPattern) =>
+                    outerPattern.slice(1, -1)
                 )
+
+                // Step 2:
+                //     For each pattern, get array of possibilities
+                //     Create a dictionary matching patterns to assigned (top-level) variables
+                //     Do not resolve pattern dots yet
+
+                const patternValueDict = innerPatterns.reduce((acc, pattern) => {
+                    const firstPattern = pattern.split(".")[0]
+                    const localValue = getDottedPath(
+                        { ...innerVars, ...context },
+                        firstPattern
+                    )
+
+                    return {
+                        ...acc,
+                        [firstPattern]: Array.isArray(localValue)
+                            ? localValue
+                            : [localValue],
+                    }
+                }, {})
+
+                // Step 3: Resolve pattern-data dictionary of combinations
+
+                const combinations = crossObject(patternValueDict)
+
+                // Step 4: Create permuted URL dictionary with assigned variable contexts
+
+                const finished_urls = combinations.map((dataCombination) => {
+                    let url = key
+                    innerPatterns.forEach((pattern) => {
+                        const instanceData = getDottedPath(dataCombination, pattern)
+                        const stringFragment = instanceData
+                            .toLowerCase()
+                            .split(" ")
+                            .join("_")
+                        url = url.replace(`[${pattern}]`, stringFragment)
+                    })
+                    return { url, instances: dataCombination }
+                })
+
+                // Step 5: For each url, recurse in and pass context
+
+                finished_urls.forEach(({ url, instances }) => {
+                    paths.push({
+                        path: parentPath + url,
+                        context: {
+                            ...context,
+                            ...innerVars,
+                            ...instances,
+                        },
+                    })
+                })
+            } else {
+                paths.push({
+                    path: parentPath + key,
+                    context: {
+                        ...context,
+                        ...innerVars,
+                    },
+                })
             }
 
             paths.map(({ path, context }) => {
-                recursePaths(
-                    path,
-                    pathData,
-                    context
-                )
+                recursePaths(path, pathData, context)
             })
         })
 }
 
 recursePaths("", pages, { data })
-
-// + apply template if exists
-// + for each path ('/...')
-//   - define path's internal variables
-//   - find path template patterns
-//   - replace path patterns with values to create set of true paths
-//     - for each path, define context based on local variables, pass along
